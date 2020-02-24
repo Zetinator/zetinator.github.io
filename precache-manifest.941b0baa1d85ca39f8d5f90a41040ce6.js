c2c9dda1800d780b1768057c0ecc270e370b76e3
Zetinator <erick_zetina@hotmail.com>
2020-02-24 01:40:02 -0600

    Updates

---
+++ precache-manifest.941b0baa1d85ca39f8d5f90a41040ce6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09c37ac38a7fd0c372ad5693fb6b8c73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d13c917f4aa564a5aa23",
+    "url": "/static/css/main.88b42d5b.chunk.css"
+  },
+  {
+    "revision": "fc9816f0d05fa6fd8ef5",
+    "url": "/static/js/2.82f4336c.chunk.js"
+  },
+  {
+    "revision": "6f5f1c4a5f7f6e382b8aeca1b4ac8d96",
+    "url": "/static/js/2.82f4336c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d13c917f4aa564a5aa23",
+    "url": "/static/js/main.627b9d66.chunk.js"
+  },
+  {
+    "revision": "17bbe1f033c8c85ec8d6",
+    "url": "/static/js/runtime-main.a48a651a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
