c876f36a3926bd1a5ceed499e0c3b9b6972d021c
Zetinator <erick_zetina@hotmail.com>
2020-02-24 17:14:07 -0600

    Updates

---
+++ precache-manifest.8ad1d55d86c4848c1ed7d163ff93a7c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9013f40695830c2f3400e520417ab7e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cfed5ac51eb810222b1",
+    "url": "/static/css/main.1f9ce4c7.chunk.css"
+  },
+  {
+    "revision": "7ee79b501e4e781a6d2a",
+    "url": "/static/js/2.bb0752d3.chunk.js"
+  },
+  {
+    "revision": "6f5f1c4a5f7f6e382b8aeca1b4ac8d96",
+    "url": "/static/js/2.bb0752d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cfed5ac51eb810222b1",
+    "url": "/static/js/main.2d0d96e4.chunk.js"
+  },
+  {
+    "revision": "17bbe1f033c8c85ec8d6",
+    "url": "/static/js/runtime-main.a48a651a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7419d9cf8aa1041a7239791616407f25.js"
+  "/precache-manifest.8ad1d55d86c4848c1ed7d163ff93a7c7.js"
 );
 
 self.addEventListener('message', (event) => {
